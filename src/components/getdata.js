a364f0f88df4d3568814598bdb5d7ebe099f655d
Marek-Kosmowski <kosmowski.marek@gmail.com>
2023-03-08 21:49:15 +0100

    Remove unnecessary code

--- src/components/getdata.js
+++ src/components/getdata.js
@@ -1,22 +1,3 @@
-// a7866bf01afc625f8d88f4c3b82439d0 -api key
-
-
-
-// async function findGif() {
-//     try {
-//         let value = input.value;
-//         let searchUrl = `https://api.giphy.com/v1/gifs/search?q=${value}&api_key=YqvV0fHXSVqa2NO9r8t0tp1ziLXfZtKI&limit=1`;
-//         const response = await fetch(searchUrl);
-//         const dataRes = await response.json();
-//         img.src = dataRes.data[0].images.original.url;
-//         console.log(dataRes)
-//     } catch (err) {
-//         alert('Something went wrong!')
-//         console.log(err)
-//     }
-
-// }
-
 async function getCityCoordinates() {
     try {
         const url = 'http://api.openweathermap.org/geo/1.0/direct?q=warsaw,&limit=1&appid=a7866bf01afc625f8d88f4c3b82439d0'
