e38174911e0e6006060d75530bea822de115a53f
Marek-Kosmowski <kosmowski.marek@gmail.com>
2023-03-10 16:30:09 +0100

    Create eventListener for input

--- src/components/getdata.js
+++ src/components/getdata.js
@@ -1,23 +1,33 @@
-// a7866bf01afc625f8d88f4c3b82439d0 - api id
+const input = document.querySelector('.input-city');
 
-// http://api.openweathermap.org/geo/1.0/direct?q=Warsaw&limit=1&appid=a7866bf01afc625f8d88f4c3b82439d0;
 
-// http://api.openweathermap.org/geo/1.0/direct?q=London&limit=5&appid={API key}
-
-// https://api.openweathermap.org/data/3.0/onecall?lat=33.44&lon=-94.04&exclude=hourly,daily&appid=a7866bf01afc625f8d88f4c3b82439d0
-
-async function getCoordinates(coord) {
-    const url = `http://api.openweathermap.org/geo/1.0/direct?q=Warsaw&limit=1&appid=a7866bf01afc625f8d88f4c3b82439d0`
+async function getCoordinates(data) {
+    const url = `http://api.openweathermap.org/geo/1.0/direct?q=${data}&limit=1&appid=a7866bf01afc625f8d88f4c3b82439d0`
     const getData = await fetch(url);
     const dataJson = await getData.json();
     const lat = dataJson[0].lat
     const lon = dataJson[0].lon
     console.log(parseNum(lat), parseNum(lon));
-    coord = ``
+    coord = `lat=${parseNum(lat)}&lon=${parseNum(lon)}`
+
+    getForecast(coord)
+}
+
+
+async function getForecast(coord) {
+    const forecastUrl = `https://api.openweathermap.org/data/2.5/weather?${coord}&appid=a7866bf01afc625f8d88f4c3b82439d0`
+    const getData = await fetch(forecastUrl);
+    const respJson = await getData.json();
+    console.log(respJson);
 
 }
-getcoordinates()
 
 function parseNum(num) {
     return Number.parseFloat(num).toFixed(2)
 }
+
+input.addEventListener('keypress', (e) => {
+    if (e.key === 'Enter') {
+        getCoordinates(input.value)
+    }
+})
