7af38173512c7bbe1389eef5018fdc05ed5120a1
Marek-Kosmowski <kosmowski.marek@gmail.com>
2023-03-10 20:47:54 +0100

    Add selectors in domhandling.js

---
+++ src/components/domhandling.js
@@ -0,0 +1,7 @@
+const cityTitle = document.querySelector('.city-title');
+const cityTemperature = document.querySelector('.city-temperature');
+const cityTempMin = document.querySelector('.city-temp-min');
+const cityTempMax = document.querySelector('.city-temp-max');
+const cityTempFeels = document.querySelector('.city-temp-feels');
+const cityHumidity = document.querySelector('.city-humidity');
+const cityPressure = document.querySelector('.city-pressure');
